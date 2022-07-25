4604780fc3b36b658d0bd5f4984ab8c9b524c2b1
Alina <alina.laktion@gmail.com>
2022-07-25 15:07:01 +0300

    add Search Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
 import React, { useState } from 'react';
-import {Button, Htag, Input, P, Rating, Tag} from '../components';
+import {Button, Htag, Input, P, Rating, Tag, Textarea} from '../components';
 import { withLayout } from '../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../interfaces/menu.interface';
@@ -21,7 +21,8 @@ function Home({ menu }: HomeProps): JSX.Element {
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-			<Input placeholder={'Name'}/>
+			<Input placeholder='Name'/>
+			<Textarea placeholder='Text'/>
 		</>
 	);
 }
