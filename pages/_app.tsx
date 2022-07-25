ed93d81d61145f2d6fd5669788f7fe97969f4cdd
Alina <alina.laktion@gmail.com>
2022-07-25 11:04:41 +0300

    Product page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
+import {AppProps} from "next/dist/shared/lib/router/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>Courses</title>
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
+                  rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+    </>;
 }
 
-export default MyApp
+export default MyApp;
