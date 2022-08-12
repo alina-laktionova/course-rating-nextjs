229a1a262fc9a61b21d489e43a521e3918cb69c8
alina-laktionova <alina.laktion@gmail.com>
2022-08-12 16:10:56 +0300

    add meta tags and husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,15 @@ import Head from 'next/head';
 import React from 'react';
 import {AppProps} from "next/dist/shared/lib/router/router";
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
+function MyApp({Component, pageProps, router}: AppProps): JSX.Element {
     return <>
         <Head>
-            <title>Courses</title>
+            <title>OWL top</title>
             <link rel="icon" href="/favicon.ico"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap"
                   rel="stylesheet"/>
+            <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
         </Head>
         <Component {...pageProps} />
     </>;
